e01c433851ec842438f3abfff583ccb4d804e4c2
Simon <168045868+simon-javier@users.noreply.github.com>
2025-12-24 14:20:39 +0800

    add conform plugin

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -2,108 +2,97 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Groups defined once at the top
-local format_is_group = augroup("FormatOnSaveGroup", { clear = true })
-local yank_group = augroup('YankGroup', { clear = true })
-local comment_group = augroup('CommentGroup', { clear = true })
-local file_group = augroup('FileGroup', { clear = true })
+local yank_group = augroup("YankGroup", { clear = true })
+local comment_group = augroup("CommentGroup", { clear = true })
+local file_group = augroup("FileGroup", { clear = true })
 
 autocmd("TextYankPost", {
-    group = yank_group,
-    callback = function()
-        vim.hl.on_yank()
-    end,
+	group = yank_group,
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 autocmd("BufEnter", {
-    group = comment_group,
-    pattern = "*",
-    command = "set fo-=c fo-=r fo-=o",
+	group = comment_group,
+	pattern = "*",
+	command = "set fo-=c fo-=r fo-=o",
 })
 
 autocmd("FileType", {
-    group = file_group,
-    callback = function()
-        local exclude = { "oil", "qf", "netrw", "lazy", "mason_backdrop", "blink-cmp-menu", "fidget", "snacks" }
-        if vim.tbl_contains(exclude, vim.bo.filetype) then
-            return
-        end
+	group = file_group,
+	callback = function()
+		local exclude = { "oil", "qf", "netrw", "lazy", "mason_backdrop", "blink-cmp-menu", "fidget", "snacks" }
+		if vim.tbl_contains(exclude, vim.bo.filetype) then
+			return
+		end
 
+		local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+		if not lang then
+			return
+		end
 
-        local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
-        if not lang then
-            return
-        end
+		local ok, _ = pcall(vim.treesitter.start)
+		if ok then
+			vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			vim.wo[0][0].foldmethod = "expr"
 
-        local ok, _ = pcall(vim.treesitter.start)
-        if ok then
-            vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-            vim.wo[0][0].foldmethod = 'expr'
-
-            -- Only set indent if nvim-treesitter is actually loaded
-            local has_ts, _ = pcall(require, 'nvim-treesitter')
-            if has_ts then
-                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            end
-        end
-    end
+			-- Only set indent if nvim-treesitter is actually loaded
+			local has_ts, _ = pcall(require, "nvim-treesitter")
+			if has_ts then
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end
+		end
+	end,
 })
 
 autocmd("User", {
-    pattern = "OilActionsPost",
-    callback = function(event)
-        if event.data.actions[1].type == "move" then
-            Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
-        end
-    end,
+	pattern = "OilActionsPost",
+	callback = function(event)
+		if event.data.actions[1].type == "move" then
+			Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+		end
+	end,
 })
 
-autocmd('LspAttach', {
-    group = augroup('kickstart-lsp-attach', { clear = false }),
-    callback = function(event)
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if not client then return end -- Safety check
+autocmd("LspAttach", {
+	group = augroup("kickstart-lsp-attach", { clear = false }),
+	callback = function(event)
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if not client then
+			return
+		end -- Safety check
 
-        local function client_supports_method(cl, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-                return cl:supports_method(method, bufnr)
-            else
-                return cl.supports_method(method, { bufnr = bufnr })
-            end
-        end
+		local function client_supports_method(cl, method, bufnr)
+			if vim.fn.has("nvim-0.11") == 1 then
+				return cl:supports_method(method, bufnr)
+			else
+				return cl.supports_method(method, { bufnr = bufnr })
+			end
+		end
 
-        -- Document Highlight Logic
-        if client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-            local highlight_augroup = augroup('kickstart-lsp-highlight', { clear = false })
-            autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                buffer = event.buf,
-                group = highlight_augroup,
-                callback = vim.lsp.buf.document_highlight,
-            })
+		-- Document Highlight Logic
+		if client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+			local highlight_augroup = augroup("kickstart-lsp-highlight", { clear = false })
+			autocmd({ "CursorHold", "CursorHoldI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.document_highlight,
+			})
 
-            autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                buffer = event.buf,
-                group = highlight_augroup,
-                callback = vim.lsp.buf.clear_references,
-            })
+			autocmd({ "CursorMoved", "CursorMovedI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.clear_references,
+			})
 
-            autocmd('LspDetach', {
-                group = augroup('kickstart-lsp-detach', { clear = true }),
-                callback = function(event2)
-                    vim.lsp.buf.clear_references()
-                    vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-                end,
-            })
-        end
-
-        -- Formatting Logic (Updated to use helper)
-        if client_supports_method(client, vim.lsp.protocol.Methods.textDocument_formatting, event.buf) then
-            autocmd("BufWritePre", {
-                buffer = event.buf,
-                group = format_is_group,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = event.buf, async = false })
-                end
-            })
-        end
-    end,
+			autocmd("LspDetach", {
+				group = augroup("kickstart-lsp-detach", { clear = true }),
+				callback = function(event2)
+					vim.lsp.buf.clear_references()
+					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+				end,
+			})
+		end
+	end,
 })
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,41 +1,41 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
-vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set('n', 'Q', '<nop>')
-vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 -- vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 -- vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 -- vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode Action' })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode Action" })
 
-vim.keymap.set('n', '<leader>sr', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set('v', '<leader>sr', [[y:%s/\V<C-r>=escape(@", '/\')<CR>/<C-r>=escape(@", '/\')<CR>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("v", "<leader>sr", [[y:%s/\V<C-r>=escape(@", '/\')<CR>/<C-r>=escape(@", '/\')<CR>/gI<Left><Left><Left>]])
