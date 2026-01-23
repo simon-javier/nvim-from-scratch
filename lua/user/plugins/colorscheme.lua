3665eab7ecf43c95a12afcdb4ff6ac7fd904cf91
aeyze <simonjavier812+github@gmail.com>
2026-01-23 22:36:11 +0800

    feat: change colorscheme to gruvbox-material

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
-    {
-      "rebelot/kanagawa.nvim",
-      priority = 1000,
-      config = function()
-          vim.cmd.colorscheme 'kanagawa'
-      end
-    }
+	{
+		"rebelot/kanagawa.nvim",
+		enabled = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_enable_italic = true
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
 }
